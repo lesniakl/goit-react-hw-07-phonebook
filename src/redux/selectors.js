353720c7114a140c3400d16d881d3b55c32f0cd9
Łukasz Leśniak <lukaszlesniaksw@gmail.com>
2023-12-08 17:45:51 +0100

    Modify selectors

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,2 +1,16 @@
-export const getContacts = state => state.contacts;
-export const getFilter = state => state.filter;
+import { createSelector } from '@reduxjs/toolkit';
+
+export const selectContacts = state => state.contacts.items;
+export const selectLoading = state => state.contacts.isLoading;
+export const selectFilter = state => state.filter;
+
+export const selectFiltered = createSelector(
+  [selectContacts, selectFilter],
+  (contacts, filter) => {
+    const filterLower = filter.toLowerCase();
+    return contacts.filter(contact => {
+      const nameLower = contact.name.toLowerCase();
+      return nameLower.includes(filterLower);
+    });
+  }
+);
