e0510d10d50528d691dee5323ccc69962e273379
Łukasz Leśniak <lukaszlesniaksw@gmail.com>
2023-12-08 17:46:46 +0100

    Change used selector, add loader

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,29 +2,18 @@ import Contact from 'components/Contact/Contact';
 import React from 'react';
 import css from './ContactList.module.css';
 import { useSelector } from 'react-redux';
-import { getContacts, getFilter } from '../../redux/selectors';
+import { selectFiltered, selectLoading } from '../../redux/selectors';
+import Loader from 'components/Loader/Loader';
 
 export default function ContactList() {
-  const contacts = useSelector(getContacts);
-  const filter = useSelector(getFilter);
-
-  const filterContacts = contactList => {
-    if (!filter) {
-      return contactList;
-    }
-    const filterLower = filter.toLowerCase();
-    return contactList.filter(contact => {
-      const nameLower = contact.name.toLowerCase();
-      return nameLower.includes(filterLower);
-    });
-  };
-
-  const filteredContacts = filterContacts(contacts);
+  const contacts = useSelector(selectFiltered);
+  const isLoading = useSelector(selectLoading);
 
   return (
     <ul className={css.contactList}>
-      {contacts.length > 0 &&
-        filteredContacts.map(contact => {
+      {isLoading && <Loader />}
+      {!isLoading &&
+        contacts.map(contact => {
           return (
             <Contact
               key={contact.id}
