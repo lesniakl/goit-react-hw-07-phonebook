1b4de5c30578f0d189214198292866f49868c1c9
Łukasz Leśniak <lukaszlesniaksw@gmail.com>
2023-12-08 17:45:58 +0100

    Create Loader component

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ThreeDots } from 'react-loader-spinner';
+
+export default function Loader() {
+  return (
+    <ThreeDots
+      height="80"
+      width="80"
+      radius="9"
+      color="red"
+      ariaLabel="loading"
+    />
+  );
+}
